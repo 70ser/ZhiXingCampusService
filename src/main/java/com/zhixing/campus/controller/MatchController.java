b79b2c65a263db879e84827930a47bab6e3d018c
xiaoming <2261046051@qq.com>
2023-04-14 23:38:27 +0800

    再次修改完善自习匹配
    可以匹配分页和无地址的

--- src/main/java/com/zhixing/campus/controller/MatchController.java
+++ src/main/java/com/zhixing/campus/controller/MatchController.java
@@ -1,6 +1,7 @@
 package com.zhixing.campus.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zhixing.campus.common.Result;
 import com.zhixing.campus.entity.Time;
 import com.zhixing.campus.service.ITimeService;
@@ -19,10 +20,12 @@ public class MatchController {
 
     //通过自习id号来匹配：寻找学伴
     @GetMapping("/get")
-    public Result matchpeople(@RequestParam Integer id){
+    public Result matchpeople(@RequestParam Integer id,@RequestParam Integer pageNum,
+                              @RequestParam Integer pageSize){
         Time time = timeService.getById(id);
         QueryWrapper<Time> wrapper = new QueryWrapper<Time>();
-        wrapper.eq("address",time.getAddress());//地址匹配
+        //wrapper.eq("address",time.getAddress());//地址匹配
+        wrapper.between("address",time.getAddress(),"无");
         wrapper.between("timestart",time.getTimestart().minusMinutes(30),time.getTimestart());//通过开始时间前30min之内匹配
         wrapper.ne("id",id);//排除自己的自习信息
         wrapper.orderByDesc("timestart");//按时间倒序排列
@@ -31,7 +34,8 @@ public class MatchController {
         if(list.size()==0){//通过list的长度判断结果
             return Result.error("400","未匹配到合适的学伴!");
         }else {
-            return Result.success(list.get(0));
+            return Result.success(timeService.page(new Page<>(pageNum, pageSize), wrapper));
         }
     }
+
 }
